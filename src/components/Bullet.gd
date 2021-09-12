b31a991fa9d089c3b8b57dc01d68a96d82751fbb
Mastermori <mastermoriii@gmail.com>
2021-09-12 02:51:52 +0200

    Updated BulletSpawner, now uses BulletEmitter resource

--- src/components/Bullet.gd
+++ src/components/Bullet.gd
@@ -1,10 +1,14 @@
 class_name Bullet
 extends Node2D
 
+const MAX_DISTANCE := 1000
+
 # Internal state
 var dir: Vector2 = Vector2.RIGHT
 var speed: float = 1.0
 var shooting: bool = false
+
+onready var start_pos := global_position
 
 func _ready() -> void:
 	shooting = true
@@ -16,3 +20,5 @@ func setup(dir, speed) -> void:
 func _physics_process(delta: float) -> void:
 	if shooting:
 		position += dir * speed * delta
+		if start_pos.distance_to(global_position) > MAX_DISTANCE:
+			queue_free()
--- src/components/BulletSpawner.gd
+++ src/components/BulletSpawner.gd
@@ -3,16 +3,17 @@ extends Position2D
 
 # Scene to use as a bullet, it's script needs to extend Bullet
 export var bullet_scene: PackedScene
-# random offset to the angle of the bullets in degree (dir +- this angle)
-export var random_angle: float = 10
 # time between shots
-export var shot_delay: float = .2
+export var shot_delay: float = .1
 # speed of the emitted bullets
 export var bullet_speed: float = 400
+# the emitter to be used for spawning bullets (controls behaviour)
+export var emitter: Resource
 
 # offset to the rotation, added to BulletSpawners rotation
 var rotation_offset: float = 0
-var shooting: bool = false
+# casted version of emitter
+var bullet_emitter: BulletEmitter
 
 onready var timer := Timer.new()
 
@@ -22,6 +23,12 @@ func _ready() -> void:
 	timer.one_shot = false
 	timer.autostart = false
 	timer.connect("timeout", self, "_shoot")
+	if emitter is BulletEmitter:
+		bullet_emitter = emitter
+		bullet_emitter.setup(self, bullet_scene)
+	else:
+		printerr("emitter in BulletSpawner is not a BulletEmitter!")
+		queue_free()
 
 func set_shooting(val: bool) -> void:
 	if timer.is_stopped():
@@ -32,12 +39,5 @@ func set_shooting(val: bool) -> void:
 			timer.stop()
 
 func _shoot() -> void:
-	var bullet: Bullet = bullet_scene.instance()
-	var shoot_dir = Vector2.RIGHT.rotated(
-		rotation + rotation_offset +
-		deg2rad(rand_range(-random_angle, random_angle))
-	).normalized()
-	bullet.setup(shoot_dir, bullet_speed)
-	bullet.set_as_toplevel(true)
-	bullet.global_position = global_position
-	add_child(bullet)
+	var shoot_dir = Vector2.RIGHT.rotated(rotation + rotation_offset).normalized()
+	emitter.shoot(global_position, shoot_dir, bullet_speed)
