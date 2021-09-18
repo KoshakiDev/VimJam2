891d72322962bb9c739ce679367121ac4f577e42
Xanthos Ware <xanthosgardenstudios@gmail.com>
2021-09-18 18:35:57 +0600

    minor code-related stuff

--- src/cowboyBoss/cowboyBoss.gd
+++ src/cowboyBoss/cowboyBoss.gd
@@ -2,6 +2,20 @@ extends Node2D
 
 
 onready var anim_player := $AnimationPlayer
+onready var gun = $Gun
+onready var vision_area = $VisionArea
+
+
+var rotation_speed
 
 func _ready():
 	anim_player.play("idle")
+
+func rotateToTarget(target, delta):
+	var direction = (target.global_position - global_position)
+	var angle_to = gun.transform.x.angle_to(direction)
+	gun.rotate(sign(angle_to) * min(delta * rotation_speed, abs(angle_to)))
+
+
+func _on_VisionArea_body_entered(body, delta):
+	rotateToTarget(body, delta)
